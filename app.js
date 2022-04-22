e8390c6899a7ad8685cb537400e2d07992a6a338
mgrzhdnv <mgrazhdanov@gmail.com>
2022-04-22 16:11:11 +0300

    added note names to keys

--- app.js
+++ app.js
@@ -5,67 +5,113 @@ const naturalNotes = ["C", "D", "E", "F", "G", "A", "B"];
 const naturalNotesSharps = ["C", "D", "F", "G", "A"];
 const naturalNotesFlats = ["D", "E", "G", "A", "B"];
 
-const range = ["A0", "C8"];
-
-const piano = document.getElementById("piano");
+const range = ["A2", "C5"];
 
 const app = {
   setupPiano() {
+    const piano = document.getElementById("piano");
     const allNaturalNotes = this.getAllNaturalNotes(range);
     const pianoWidth = allNaturalNotes.length * whiteKeyWidth;
 
     const SVG = this.createMainSVG(pianoWidth, pianoHeight);
-    // Add main SVG to piano div
-    piano.appendChild(SVG);
 
     // Add white keys
     let whiteKeyPosX = 0;
-    for (let i = 0; i < allNaturalNotes.length; i++) {
+
+    allNaturalNotes.forEach((noteName) => {
+      const whiteKeyTextGroup = utils.createSVGElement("g");
       const whiteKey = this.createKey({
         className: "white-key",
         width: whiteKeyWidth,
         height: pianoHeight,
       });
-      whiteKey.setAttribute("x", whiteKeyPosX);
-      whiteKey.setAttribute("data-note-name", allNaturalNotes[i]);
+      const text = utils.createSVGElement("text");
+
+      utils.addTextContent(text, noteName);
+      utils.setAttributes(whiteKeyTextGroup, {
+        width: whiteKeyWidth,
+      });
+      utils.setAttributes(text, {
+        x: whiteKeyPosX + whiteKeyWidth / 2,
+        y: 380,
+        "text-anchor": "middle",
+      });
+      utils.setAttributes(whiteKey, {
+        x: whiteKeyPosX,
+        "data-note-name": noteName,
+      });
+
+      text.classList.add("white-key-text");
+      whiteKeyTextGroup.appendChild(whiteKey);
+      whiteKeyTextGroup.appendChild(text);
+      SVG.appendChild(whiteKeyTextGroup);
+      // Increment spacing between keys
       whiteKeyPosX += whiteKeyWidth;
-      SVG.appendChild(whiteKey);
-    }
+    });
+
     // Add black keys
     let blackKeyPosX = 60;
     allNaturalNotes.forEach((naturalNote, index, array) => {
+      // If last iteration of keys, do not add black key
+      if (index === array.length - 1) {
+        return;
+      }
+
+      const blackKeyTextGroup = utils.createSVGElement("g");
+
       const blackKey = this.createKey({
         className: "black-key",
         width: whiteKeyWidth / 2,
         height: pianoHeight / 1.6,
       });
-      blackKey.setAttribute("x", blackKeyPosX);
+      const flatNameText = utils.createSVGElement("text");
+      const sharpNameText = utils.createSVGElement("text");
+
+      utils.setAttributes(blackKeyTextGroup, {
+        width: whiteKeyWidth / 2,
+      });
 
       for (let i = 0; i < naturalNotesSharps.length; i++) {
         let naturalSharpNoteName = naturalNotesSharps[i];
         let naturalFlatNoteName = naturalNotesFlats[i];
         if (naturalSharpNoteName === naturalNote[0]) {
-          blackKey.setAttribute(
-            "data-sharp-name",
-            `${naturalSharpNoteName}#${naturalNote[1]}`
-          );
-          blackKey.setAttribute(
-            "data-flat-name",
-            `${naturalFlatNoteName}b${naturalNote[1]}`
-          );
+          utils.setAttributes(blackKey, {
+            x: blackKeyPosX,
+            "data-sharp-name": `${naturalSharpNoteName}#${naturalNote[1]}`,
+            "data-flat-name": `${naturalFlatNoteName}b${naturalNote[1]}`,
+          });
+          utils.setAttributes(sharpNameText, {
+            "text-anchor": "middle",
+            y: 215,
+            x: blackKeyPosX + whiteKeyWidth / 4,
+          });
+          utils.setAttributes(flatNameText, {
+            "text-anchor": "middle",
+            y: 235,
+            x: blackKeyPosX + whiteKeyWidth / 4,
+          });
+
+          utils.addTextContent(sharpNameText, `${naturalSharpNoteName}♯`);
+          utils.addTextContent(flatNameText, `${naturalFlatNoteName}♭`);
+
+          flatNameText.classList.add("black-key-text");
+          sharpNameText.classList.add("black-key-text");
+
           // Add double spacing after D# and A#
           if (naturalSharpNoteName === "D" || naturalSharpNoteName === "A") {
             blackKeyPosX += whiteKeyWidth * 2;
           } else {
             blackKeyPosX += whiteKeyWidth;
           }
-          // If last iteration of keys, do not add black key
-          if (index !== array.length - 1) {
-            SVG.appendChild(blackKey);
-          }
+          blackKeyTextGroup.appendChild(blackKey);
+          blackKeyTextGroup.appendChild(flatNameText);
+          blackKeyTextGroup.appendChild(sharpNameText);
         }
       }
+      SVG.appendChild(blackKeyTextGroup);
     });
+    // Add main SVG to piano div
+    piano.appendChild(SVG);
   },
   // createOctave(octaveNumber) {
   //   const octave = utils.createSVGElement("g");
@@ -79,8 +125,10 @@ const app = {
   createKey({ className, width, height }) {
     const key = utils.createSVGElement("rect");
     key.classList.add(className);
-    key.setAttribute("width", width);
-    key.setAttribute("height", height);
+    utils.setAttributes(key, {
+      width: width,
+      height: height,
+    });
     return key;
   },
   getAllNaturalNotes([firstNote, lastNote]) {
@@ -122,11 +170,14 @@ const app = {
   },
   createMainSVG(pianoWidth, pianoHeight) {
     const svg = utils.createSVGElement("svg");
-    svg.setAttribute("width", "100%");
-    svg.setAttribute("version", "1.1");
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
-    svg.setAttribute("viewBox", `0 0 ${pianoWidth} ${pianoHeight}`);
+
+    utils.setAttributes(svg, {
+      width: "100%",
+      version: "1.1",
+      xmlns: "http://www.w3.org/2000/svg",
+      "xmlns:xlink": "http://www.w3.org/1999/xlink",
+      viewBox: `0 0 ${pianoWidth} ${pianoHeight}`,
+    });
 
     return svg;
   },
@@ -136,6 +187,14 @@ const utils = {
   createSVGElement(el) {
     const element = document.createElementNS("http://www.w3.org/2000/svg", el);
     return element;
+  },
+  setAttributes(el, attrs) {
+    for (let key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  },
+  addTextContent(el, content) {
+    el.textContent = content;
   },
 };
 
